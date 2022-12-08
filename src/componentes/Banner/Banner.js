9d405a8066ab174086317c5d7a67200570657b7c
otaviordc <otavioribeirodecastro@hotmail.com>
2022-12-07 23:06:26 -0300

    Second commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner () {
+    // jsx
+    return (
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="O banner principal da página organo"/>
+        </header>
+    )
+
+}
+
+export default Banner
